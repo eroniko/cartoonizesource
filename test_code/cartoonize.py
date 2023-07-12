80df2d77d8b1234c43dc7f18f8c1905f1a786d89
eroniko <46302653+eroniko@users.noreply.github.com>
2023-07-12 19:33:16 +0300

    Update cartoonize.py

--- test_code/cartoonize.py
+++ test_code/cartoonize.py
@@ -22,7 +22,7 @@ def resize_crop(image):
     return image
     
 
-def cartoonize(load_folder, save_folder, model_path):
+def cartoonize_single_file(load_path, save_folder, model_path):
     input_photo = tf.compat.v1.placeholder(tf.float32, [1, None, None, 3])
     network_out = network.unet_generator(input_photo)
     final_out = guided_filter.guided_filter(input_photo, network_out, r=1, eps=5e-3)
@@ -37,30 +37,27 @@ def cartoonize(load_folder, save_folder, model_path):
 
     sess.run(tf.compat.v1.global_variables_initializer())
     saver.restore(sess, tf.train.latest_checkpoint(model_path))
-    name_list = os.listdir(load_folder)
-    for name in tqdm(name_list):
-        try:
-            load_path = os.path.join(load_folder, name)
-            save_path = os.path.join(save_folder, name)
-            image = cv2.imread(load_path)
-            image = resize_crop(image)
-            batch_image = image.astype(np.float32)/127.5 - 1
-            batch_image = np.expand_dims(batch_image, axis=0)
-            output = sess.run(final_out, feed_dict={input_photo: batch_image})
-            output = (np.squeeze(output)+1)*127.5
-            output = np.clip(output, 0, 255).astype(np.uint8)
-            cv2.imwrite(save_path, output)
-        except:
-            print('cartoonize {} failed'.format(load_path))
-
-
     
+    try:
+        save_path = os.path.join(save_folder, os.path.basename(load_path))
+        image = cv2.imread(load_path)
+        image = resize_crop(image)
+        batch_image = image.astype(np.float32)/127.5 - 1
+        batch_image = np.expand_dims(batch_image, axis=0)
+        output = sess.run(final_out, feed_dict={input_photo: batch_image})
+        output = (np.squeeze(output)+1)*127.5
+        output = np.clip(output, 0, 255).astype(np.uint8)
+        cv2.imwrite(save_path, output)
+    except:
+        print('cartoonize {} failed'.format(load_path))
 
+            
 if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_image", type=str, help="path to your input image")
+    parser.add_argument("--output_folder", type=str, help="path to output folder")
+    args = parser.parse_args()
+
     model_path = 'saved_models'
-    load_folder = 'test_images'
-    save_folder = 'cartoonized_images'
-    if not os.path.exists(save_folder):
-        os.mkdir(save_folder)
-    cartoonize(load_folder, save_folder, model_path)
-    
+    cartoonize_single_file(args.input_image, args.output_folder, model_path)
